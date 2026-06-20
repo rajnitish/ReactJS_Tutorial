9adc374428d47b0fbe7d84708c664ed35c620078
Nitish Raj <raj.nitp@gmail.com>
2026-06-20 12:31:48 +0530

    Card added

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      
+      <div className="card">
+        <h2>Card Title</h2>
+        <p>This is a card component.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
